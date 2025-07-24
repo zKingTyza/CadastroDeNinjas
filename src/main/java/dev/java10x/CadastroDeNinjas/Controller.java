9677885151dea35891fd5d23fdb11cdd5367f7a7
Alex Zaleski <alexzaleski159@gmail.com>
2025-07-23 21:12:25 -0300

    Criado a controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeNinjas;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem na minha rota";
+    }
+}
