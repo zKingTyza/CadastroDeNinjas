117f349ac819e3cfa6f30ad99ed0b9d735aaf9da
Alex Zaleski <alexzaleski159@gmail.com>
2025-08-05 15:08:32 -0300

    fix: Adiciona NinjaRepository para interface

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
