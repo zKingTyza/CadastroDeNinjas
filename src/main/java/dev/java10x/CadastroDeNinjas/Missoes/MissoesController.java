574195e2ee7ae34d238b3466b71d29f1feded0d4
Alex Zaleski <alexzaleski159@gmail.com>
2025-08-12 00:49:07 -0300

    feat: Cria as rotas do Missoes controller com mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,36 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+//LocalHOST:8080
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    //REQUISIÇÃO
+    // GET -- MANDAR UMA REQUISIÇAO
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas com sucesso";
+    }
+
+    // POST -- CRIAR AS MISSOES
+    @PostMapping("criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso";
+    }
+
+    // PUT -- ALTERAR MISSOES
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+
+    // DELETE -- DELETAR MISSOES
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
 }
